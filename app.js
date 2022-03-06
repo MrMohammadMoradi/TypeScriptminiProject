c9c24b72ae862f397318c55d6ed0b136e09a9479
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-03-06 23:52:48 +0330

    add code for show Information(app.js & style.css)

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ let rowNumber = 0,
     userName = document.getElementById('getName'),
     userCity = document.getElementById('getCity'),
     userCountry = document.getElementById('getCountry'),
-    userAge = document.getElementById('getAge')
+    userAge = document.getElementById('getAge'),
+    table = document.getElementsByClassName('InforamationTable')
+
+
 
 const editRow = document.querySelector('.Edit'),
     submitBtn = document.getElementById('submit')
@@ -13,6 +16,34 @@ submitBtn.addEventListener('click', () => {
     this.country = userCountry.value
     this.age = userAge.value
 
+    rowNumber++
     
+    let trOfTable = document.createElement('tr', ''),
+        rowInTable = document.createElement('td'),
+        nameInTable = document.createElement('td'),
+        cityInTable = document.createElement('td'),
+        countryInTable = document.createElement('td'),
+        ageInTable = document.createElement('td'),
+        editInTable = document.createElement('td'),
+        deleteInTable = document.createElement('td')
+
+    rowInTable.innerHTML = rowNumber
+    nameInTable.innerHTML = name
+    cityInTable.innerHTML = city
+    countryInTable.innerHTML = country
+    ageInTable.innerHTML = age
+    editInTable.innerHTML = "Edit"
+    deleteInTable.innerHTML = "Delet"
+
+    trOfTable.appendChild(rowInTable)
+    trOfTable.appendChild(nameInTable)
+    trOfTable.appendChild(cityInTable)
+    trOfTable.appendChild(countryInTable)
+    trOfTable.appendChild(ageInTable)
+    trOfTable.appendChild(editInTable)
+    trOfTable.appendChild(deleteInTable)
+
+    document.body.appendChild(trOfTable)
+
 })
 
